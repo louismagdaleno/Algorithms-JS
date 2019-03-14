a859c159a02bfef3f45ac5b6b451510de0f43ae7
Louis Magdaleno <buildthatapp@gmail.com>
2019-03-14 16:08:15 -0700

    Refactored maxSubarraySum

--- maxSubArraySum/index.js
+++ maxSubArraySum/index.js
@@ -1,23 +1,23 @@
 // Write a function called maxSubarraySum which accepts an array of integers
 //  and a number called n. The function should calculate the maximum sum of n
 //   consecutive elements in the array.
-function maxSubarraySum(arr, target) {
-    if (arr.length < target) {
-        return null;
+function maxSubarraySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+      maxSum += arr[i];
     }
-    
-
-    
-    let tail = target - 1;
-    let sum = 0;
-    for (let i = 0 ; i < tail; i++) {
-        sum += arr[i];
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+      tempSum = tempSum - arr[i - num] + arr[i];
+      maxSum = Math.max(maxSum, tempSum);
     }
-    console.log(sum);
-}
+    return maxSum;
+  }
 
 console.log(maxSubarraySum([1,2,5,2,8,1,5],2)) // 10
-// console.log(maxSubarraySum([1,2,5,2,8,1,5],4)) // 17
-// console.log(maxSubarraySum([4,2,1,6],1)) // 6
-// console.log(maxSubarraySum([4,2,1,6,2],4)) // 13
-// console.log(maxSubarraySum([],4)) // null
+console.log(maxSubarraySum([1,2,5,2,8,1,5],4)) // 17
+console.log(maxSubarraySum([4,2,1,6],1)) // 6
+console.log(maxSubarraySum([4,2,1,6,2],4)) // 13
+console.log(maxSubarraySum([],4)) // null
