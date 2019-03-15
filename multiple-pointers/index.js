2d36c1425f1fa28814c1ecc12c87f06d010f8b8a
Louis Magdaleno <buildthatapp@gmail.com>
2019-03-15 09:55:35 -0700

    Added averagePair

--- multiple-pointers/index.js
+++ multiple-pointers/index.js
@@ -36,3 +36,30 @@ console.log(sumZero([-3,-2,-1,0,1,2,3])) // [-3,3]
 console.log(sumZero([-2,0,1,3])) // undefined
 console.log(sumZero([1,2,3])) // undefined
 console.log(sumZero([-4,-3,-2,-1,0,1,5,7])) // [-1, 1]
+
+// averagePair
+// Write a function called averagePair. Given a sorted array of integers and a target average,
+// determine if there is a pair of values in the array where the average of the pair equals the target
+// average. There may be more than one pair that matches the average target.
+
+function averagePair(arr, num){
+    // add whatever parameters you deem necessary - good luck!
+    let left = 0, right = arr.length - 1;
+    
+    while (left < right) {
+        let average = ( arr[left] + arr[right] ) / 2;
+        if (average === num) {
+            return true;
+        } else if (average > num) {
+            right --;
+        } else {
+            left ++;
+        }
+    }
+    return false;
+  }
+
+  console.log(averagePair([1,2,3], 2.5)) //true
+  console.log(averagePair([1,3,3,5,6,7,10,12,19], 8)) //true
+  console.log(averagePair([-1,0,3,4,5,6], 4.1)) //false
+  console.log(averagePair([], 4))  // false
